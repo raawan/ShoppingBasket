5bfa79dc3d1091c42bb1048399c09c0964ba727b
minal <minalfulzele@gmail.com>
2018-10-28 14:04:56 +0000

    refactored logic

--- src/com/bjss/shopping/PriceBasket.java
+++ src/com/bjss/shopping/PriceBasket.java
@@ -21,46 +21,61 @@ public class PriceBasket {
     public BigDecimal getSubTotal() {
 
         BigDecimal total = items.stream()
-                    .map(item -> ((Item) item).getPrice())
-                    .reduce(new BigDecimal("0"), (price1, price2) -> price1.add(price2));
+                .map(item -> ((Item) item).getPrice())
+                .reduce(new BigDecimal("0"), (price1, price2) -> price1.add(price2));
         return total.setScale(2, RoundingMode.HALF_UP);
     }
 
     public BigDecimal getTotal() {
 
-        BigDecimal discountedApplePrice = items.stream()
+        BigDecimal discountedApplePrice = applyAppleSpecialOffer();
+        BigDecimal discountedBreadPrice = applySoupAndBreadSpecialOffer();
+        BigDecimal allTotalWithoutBreadAndApple = getTotalExcludingSpecialOfferItem();
+        BigDecimal totalAfterDiscount = discountedBreadPrice.add(allTotalWithoutBreadAndApple).add(discountedApplePrice);
+        return totalAfterDiscount.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal applyAppleSpecialOffer() {
+        return items.stream()
                 .filter(item -> ((Item) item).getName().equalsIgnoreCase("Apple"))
                 .map(item -> ((Item) item).getPrice().multiply(new BigDecimal("0.90")))
                 .reduce(new BigDecimal("0"), (price1, price2) -> price1.add(price2));
+    }
 
-        long countSoup = items.stream()
-                .filter(item -> ((Item) item).getName().equalsIgnoreCase("Soup"))
-                .count();
-        long countBread = items.stream()
-                .filter(item -> ((Item) item).getName().equalsIgnoreCase("Bread"))
-                .count();
-
-        BigDecimal totalBread = items.stream()
-                .filter(item -> ((Item) item).getName().equalsIgnoreCase("Bread"))
-                .map(item -> ((Item) item).getPrice())
-                .reduce(new BigDecimal("0"), (price1, price2) -> price1.add(price2));
-
-        BigDecimal discountedBreadPrice=new BigDecimal("0");
-        if( countSoup >= 2 && countBread >= 1) {
-            long totaldiscountToApply = countSoup/2;
-            totaldiscountToApply= totaldiscountToApply==1?2:totaldiscountToApply;
-            discountedBreadPrice  = totalBread.divide(new BigDecimal(totaldiscountToApply));
-        } else {
-            discountedBreadPrice = totalBread;
-        }
-
-        BigDecimal allTotalWithoutBreadAndApple = items.stream()
+    private BigDecimal getTotalExcludingSpecialOfferItem() {
+        return items.stream()
                 .filter(item -> !((Item) item).getName().equalsIgnoreCase("Bread"))
                 .filter(item -> !((Item) item).getName().equalsIgnoreCase("Apple"))
                 .map(item -> ((Item) item).getPrice())
                 .reduce(new BigDecimal("0"), (price1, price2) -> price1.add(price2));
-
-        BigDecimal totalAfterDiscount = discountedBreadPrice.add(allTotalWithoutBreadAndApple).add(discountedApplePrice);
-        return totalAfterDiscount.setScale(2, RoundingMode.HALF_UP);
     }
+
+    private BigDecimal applySoupAndBreadSpecialOffer() {
+        long countSoup = getItemCount("Soup");
+        long countBread = getItemCount("Bread");
+        BigDecimal totalBread = getBreadTotal();
+        BigDecimal discountedBreadPrice;
+        if (countSoup >= 2 && countBread >= 1) {
+            long totaldiscountToApply = countSoup / 2;
+            totaldiscountToApply = totaldiscountToApply == 1 ? 2 : totaldiscountToApply;
+            discountedBreadPrice = totalBread.divide(new BigDecimal(totaldiscountToApply));
+            return discountedBreadPrice;
+        }
+        return totalBread;
+    }
+
+    private BigDecimal getBreadTotal() {
+        return  items.stream()
+                .filter(item -> ((Item) item).getName().equalsIgnoreCase("Bread"))
+                .map(item -> ((Item) item).getPrice())
+                .reduce(new BigDecimal("0"), (price1, price2) -> price1.add(price2));
+    }
+
+    private long getItemCount(String itemName) {
+        return  items.stream()
+                .filter(item -> ((Item) item).getName().equalsIgnoreCase(itemName))
+                .count();
+    }
+
+
 }
